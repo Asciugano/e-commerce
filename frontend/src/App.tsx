7bfb83eafcbd4c39958f39bf5eef5109654d9fb4
Asciugano <diaferiosamuele@gmail.com>
2025-12-15 19:59:06 +0100

    aggiunta la navbar e il react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,45 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Navbar } from './components/navbar'
+import { HomePage } from './pages/home_page'
+import RegisterPage from './pages/auth/register_page'
+import LoginPage from './pages/auth/login_page'
+import OrderPage from './pages/order_page'
+import ProductsPage from './pages/products_page'
+import UserPage from './pages/user_page'
 
 function App() {
 
   return (
-    <div>Hello World</div>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route
+          path='/'
+          element={<HomePage />}
+        />
+        <Route
+          path='/register'
+          element={<RegisterPage />}
+        />
+        <Route
+          path='/login'
+          element={<LoginPage />}
+        />
+        <Route
+          path='/orders'
+          element={<OrderPage />}
+        />
+        <Route
+          path='/products'
+          element={<ProductsPage />}
+        />
+        <Route
+          path='/user'
+          element={<UserPage />}
+        />
+      </Routes>
+    </div>
   )
 }
 
--- frontend/src/pages/user_page.tsx
+++ frontend/src/pages/user_page.tsx
@@ -1,10 +1,8 @@
-import type { User } from "../types/user.ts";
 
-export default function UserPage({ user }: { user: User }) {
+export default function UserPage() {
   return (
     <div>
       <h1>User Page</h1>
-      <p>{user.id}</p>
     </div>
   );
 }
