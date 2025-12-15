3686aaf923b0209fc43c02f794cfef1085a4a5a8
Asciugano <diaferiosamuele@gmail.com>
2025-12-15 22:09:16 +0100

    creato il authContext, e finita la navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
-import { User, Package, ShoppingCart } from "lucide-react";
+import { User, Package, ShoppingCart, LogIn, LogOut } from "lucide-react";
+import { useAuth } from "../context/AuthContext";
 
 export function Navbar() {
+
+  const { isLogged, logout } = useAuth();
+
   return (
     <nav className="mx-4 my-4 rounded-xl bg-neutral-900 shadow-lg">
       <div className="flex items-center justify-between px-6 py-3">
@@ -9,9 +13,9 @@ export function Navbar() {
         <div>
           <Link
             to="/"
-            className="text-xl font-bold tracking-wide text-amber-400 hover:text-amber-300 transition"
+            className="inline-block text-xl font-bold tracking-wide text-emerald-400 hover:text-emerald-300 hover:-translate-y-1 hover:shadow-lg transition transform duration-200"
           >
-            E
+            E-COM
           </Link>
         </div>
 
@@ -19,47 +23,49 @@ export function Navbar() {
         <div className="flex items-center space-x-6">
           {/* Links */}
           <Link
-            to="/user"
-            className="flex items-center gap-1 hover:text-amber-400 transition"
-          >
-            <User size={18} />
-            <span>Profilo</span>
-          </Link>
-
-          <Link
-            to="/orders"
-            className="flex items-center gap-1 hover:text-amber-400 transition"
-          >
-            <ShoppingCart size={18} />
-            <span>Orders</span>
-          </Link>
-
-          <Link
             to="/products"
-            className="flex items-center gap-1 hover:text-amber-400 transition"
+            className="flex items-center gap-1 hover:text-emerald-400 transition"
           >
             <Package size={18} />
             <span>Products</span>
           </Link>
 
+          <Link
+            to="/orders"
+            className="flex items-center gap-1 hover:text-emerald-400 transition"
+          >
+            <ShoppingCart size={18} />
+            <span>Orders</span>
+          </Link>
+
+          {isLogged && (
+            <Link
+              to="/user"
+              className="flex items-center gap-1 hover:text-emerald-400 transition"
+            >
+              <User size={18} />
+              <span>Profilo</span>
+            </Link>
+          )}
+
           {/* Login o Logout */}
-          {/* {!logged ? ( */}
-          {/*   <Link */}
-          {/*     to="/auth/login" */}
-          {/*     className="flex items-center gap-1 px-4 py-2 text-white rounded-lg bg-amber-400 hover:bg-amber-300 transition" */}
-          {/*   > */}
-          {/*     <LogIn size={18} /> */}
-          {/*     <span>Login</span> */}
-          {/*   </Link> */}
-          {/* ) : ( */}
-          {/*   <button */}
-          {/*     onClick={handleLogout} */}
-          {/*     className="flex items-center gap-1 px-4 py-2 text-white rounded-lg bg-red-600 hover:bg-red-500 transition" */}
-          {/*   > */}
-          {/*     <LogOut size={18} /> */}
-          {/*     <span>Logout</span> */}
-          {/*   </button> */}
-          {/* )} */}
+          {!isLogged ? (
+            <Link
+              to="/login"
+              className="flex items-center gap-1 px-4 py-2 text-white rounded-lg bg-emerald-400 hover:bg-emerald-300 hover:-translate-y-1 hover:shadow-lg transition transform duration-200"
+            >
+              <LogIn size={18} />
+              <span>Login</span>
+            </Link>
+          ) : (
+            <button
+              onClick={logout}
+              className="flex items-center gap-1 px-4 py-2 text-white rounded-lg bg-red-600 hover:bg-red-500 transition"
+            >
+              <LogOut size={18} />
+              <span>Logout</span>
+            </button>
+          )}
         </div>
       </div>
     </nav>
--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,8 +1,47 @@
 import type { User } from '../types/user.ts';
-import { createContext, useContext, useState, ReactNode } from "react";
+import { createContext, useContext, useState } from "react";
+import type { ReactNode } from 'react';
 
 const AuthContext = createContext<AuthContextType | null>(null);
 
+type AuthContextType = {
+  user: User | null;
+  isLogged: boolean;
+  login: (user: User) => void;
+  logout: () => void;
+}
+
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
+
+  const login = (user: User) => {
+    setUser(user);
+    localStorage.setItem("user", JSON.stringify(user));
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        isLogged: !!user,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuth() {
+  const ctx = useContext(AuthContext);
+  if (!ctx)
+    throw new Error("use Auth deve essere usato dentro a AuthProvider");
+
+  return ctx;
 }
