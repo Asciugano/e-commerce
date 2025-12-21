82af13aa074c145cb818e69d1c92d72b775a492b
Asciugano <diaferiosamuele@gmail.com>
2025-12-21 15:55:21 +0100

    iniziato a lavorare con i prodotti nel frontend

--- frontend/src/components/product_component.tsx
+++ frontend/src/components/product_component.tsx
@@ -3,8 +3,13 @@ import type { Product } from "../types/product";
 export default function Product({ product }: { product: Product }) {
   return (
     <div>
-      <h1>Product component</h1>
-      <p>{product.id}</p>
+      {product.photos.length > 0 && (
+        <img src={product.photos[0]} alt={product.name} />
+      )}
+      <h2>{product.name}</h2>
+      <p>${product.price}</p>
+
+      {/* TODO: implementare la ricerca per seller */}
     </div>
   );
 }
--- frontend/src/pages/products_page.tsx
+++ frontend/src/pages/products_page.tsx
@@ -1,5 +1,57 @@
+import { useEffect, useState } from "react";
+import Product from "../components/product_component";
+import { AxiosError } from "axios";
+import { api } from "../lib/axios";
+import { Loader2 } from "lucide-react";
+
 export default function ProductsPage() {
+  const [products, setProducts] = useState([]);
+
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    api.get("/products/")
+      .then((res) => res.data)
+      .then((data) => setProducts(data.products))
+      .catch((e) => {
+        console.error(e);
+        const err = e as AxiosError<{ message?: string }>;
+        if (err.response?.data.message)
+          setError(err.response.data.message);
+        else if (typeof err.response?.data === "string")
+          setError(err.response?.data);
+        else
+          setError("Ops... Something went wrong");
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+
+  if (loading || error)
+    return (
+      <div className="flex items-center justify-center w-full bg-neutral-900 rounded-2xl shadow-lg px-4 py-2">
+        {loading && (
+          <Loader2 size={50} className="text-emerald-400 animate-spin" />
+        )}
+        {error && (
+          <h2 className="text-lg text-red-600">{error}</h2>
+        )}
+      </div>
+    );
+
   return (
-    <div>Products Page</div>
+    <div>
+      <div className="flex flex-col gap-3 flex-1 bg-neutral-900 rounded-2xl shadow-lg px-6 py-4">
+        {products && products.length > 0 && (
+          <div className="w-full">
+            {products.map(product => (
+              <Product product={product} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
